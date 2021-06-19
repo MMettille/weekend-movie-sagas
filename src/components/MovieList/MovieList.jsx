fd49f06b1ef13822b01ce5aea6c3b19625a89680
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-19 18:19:12 -0500

    Added Spacing to the home page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,6 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
+// Imports needed from material-ui for the Card
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+
+
 function MovieList() {
 
     const history = useHistory()
@@ -21,19 +31,27 @@ function MovieList() {
     }
 
     return (
-        <main>
+        <div>
             <h1>MovieList</h1>
             <section className="movies">
+                <Grid container spacing={5}>
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} onClick={() => handleClick(movie.id)}>
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
+                        <Grid item sm={3} >
+                            <Paper>
+                                <Card key={movie.id} onClick={() => handleClick(movie.id)}>
+                                    <CardActionArea>
+                                    <Typography gutterBottom variant="h4" component="h2">{movie.title}</Typography>
+                                    <img src={movie.poster} alt={movie.title}/>
+                                    </CardActionArea>
+                                </Card>
+                            </Paper>
+                        </Grid>
                     );
                 })}
+                </Grid>
             </section>
-        </main>
+        </div>
 
     );
 }

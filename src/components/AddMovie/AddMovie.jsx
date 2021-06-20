17e8d497acd391e1101ef4ba47e27862a181ebde
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-20 11:20:34 -0500

    Added Drop down for the genre

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -9,7 +9,18 @@ function addMovie(){
         <TextField id="standard-basic" label="Movie Title" placeholder="Movie Title"/>
         <TextField id="standard-basic" label="Movie Poster" placeholder="Movie Poster"/>
         <TextField id="standard-textarea" label="Movie Description" placeholder="Description" multiline />
-
+        <InputLabel id="demo-simple-select-label">Genre</InputLabel>
+        <Select
+          labelId="genre selection"
+          id="demo-simple-select"
+          value={}
+          onChange={}
+        >
+          <MenuItem value={}>Ten</MenuItem>
+          <MenuItem value={}>Twenty</MenuItem>
+          <MenuItem value={}>Thirty</MenuItem>
+        </Select>
+        <FormHelperText>Select the movie's genre here.</FormHelperText>
 
         </form>
         </>

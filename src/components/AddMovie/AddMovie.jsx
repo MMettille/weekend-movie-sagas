513be9c92566b86b06add226dc789ce557f0a059
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-20 11:15:10 -0500

    Started on the Add Movie Form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,8 +1,17 @@
+import TextField from '@material-ui/core/TextField';
+
 function addMovie(){
 
 
     return (
         <>
+        <form onSubmit={handleSubmit}>
+        <TextField id="standard-basic" label="Movie Title" placeholder="Movie Title"/>
+        <TextField id="standard-basic" label="Movie Poster" placeholder="Movie Poster"/>
+        <TextField id="standard-textarea" label="Movie Description" placeholder="Description" multiline />
+
+
+        </form>
         </>
     )
 }
